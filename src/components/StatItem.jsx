f9f966220cb48cec4bfcb0791e2c2719eb15e363
Ramil <Ramilmys@gmail.com>
2026-02-07 05:26:19 +0700

    Компонент отредактирован

--- src/components/StatItem.jsx
+++ src/components/StatItem.jsx
@@ -1,12 +1,14 @@
 import styles from "../styles/character-catalog.module.css";
 
-export function StatItem({ className, Icon, total, label, color }) {
+export function StatItem({ className, labelClassName, Icon, total, label, color }) {
   return (
     <div className={className}>
       <Icon className={styles["icon"]} color={color} />
       <div className={styles["stat-content"]}>
         <h4 className={styles["stat-label"]}>{total}</h4>
-        <h2 className={styles["stat-value"]}>{label}</h2>
+        <h2 className={labelClassName ? labelClassName : styles["stat-value"]}>
+          {label}
+        </h2>
       </div>
     </div>
   );
