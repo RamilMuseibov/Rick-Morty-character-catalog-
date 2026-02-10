a8c52c380a866ec3cd9389852908aa027a5ac452
Ramil <Ramilmys@gmail.com>
2026-02-11 03:38:51 +0700

    Прописаны недостающие пропсы

--- src/components/SelectItem.jsx
+++ src/components/SelectItem.jsx
@@ -1,10 +1,8 @@
-import styles from "../styles/character-catalog.module.css";
-
-export function SelectItem({ className, name, id, options }) {
+export function SelectItem({ className, name, id, options, onChange, value }) {
   return (
-    <select className={className} name={name} id={id}>
-      {options?.map((option, index) => (
-        <option key={index} value={option}>
+    <select onChange={onChange} value={value} className={className} name={name} id={id}>
+      {options?.map((option) => (
+        <option key={option} value={option}>
           {option}
         </option>
       ))}
