e8b976e441c714aed7d9aee4794c612edf31776d
Ramil <Ramilmys@gmail.com>
2026-02-13 04:26:43 +0700

    Инпут изменен на компонент

--- src/components/SearchCharacters.jsx
+++ src/components/SearchCharacters.jsx
@@ -2,16 +2,18 @@ import ReloadIcon from "../icons/ReloadIcon";
 import SearchIcon from "../icons/SearchIcon";
 import styles from "../styles/character-catalog.module.css";
 import Button from "./Button";
+import Input from "./Input";
 
 export default function SearchCharacters() {
   return (
     <div className={styles["search-container"]}>
       <div className={styles["search-field"]}>
         <SearchIcon className={styles["search-icon"]} />
-        <input
+
+        <Input
+          type={"text"}
           className={styles["input-search"]}
-          type="text"
-          placeholder="Search by name or type..."
+          placeholder={"Search by name or type..."}
         />
       </div>
 
