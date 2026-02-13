194d263c037f72395198d626987f0830f17b31dd
Ramil <Ramilmys@gmail.com>
2026-02-14 03:31:54 +0700

    Главный файл, все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import CharactersCard from "./components/CharactersCard";
 import InfoCharacters from "./components/InfoCharacters";
 import SearchCharacters from "./components/SearchCharacters";
 import SortingCharacters from "./components/SortingCharacters";
+import Button from "./components/Button";
+import ArrowLeftIcon from "./icons/ArrowLeftIcon";
+import ArrowRightIcon from "./icons/ArrowRightIcon";
+import ActiveCard from "./components/ActiveCard";
 
 export default function RickMortyCharacterCatalog() {
   const [characters, setCharacters] = useState([]);
   const [info, setInfo] = useState(null);
   const [favoritesCharacters, setFavoritesCharacters] = useState([]);
-  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [urlPage, setUrlPage] = useState(1);
   const [name, setName] = useState("");
   const [isActive, setIsActive] = useState(false);
+  const [activeCard, setActiveCard] = useState(null);
 
   const [statusFilter, setStatusFilter] = useState("All status");
   const [genderFilter, setGenderFilter] = useState("All gender");
@@ -19,10 +25,20 @@ export default function RickMortyCharacterCatalog() {
   const [sorting, setSorting] = useState("Without sorting");
 
   useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [urlPage, activeCard]);
+
+  useEffect(() => {
     async function getCharacters() {
+      const urlPage1 = urlPage * 2 - 1;
+      const urlPage2 = urlPage * 2;
+
       const urls = [
-        `https://rickandmortyapi.com/api/character?page=${page}&name=${name}`,
-        `https://rickandmortyapi.com/api/character?page=${page + 1}&name=${name}`,
+        `https://rickandmortyapi.com/api/character?page=${urlPage1}&name=${name}`,
+        `https://rickandmortyapi.com/api/character?page=${urlPage2}&name=${name}`,
       ];
 
       const responses = await Promise.all(urls.map((url) => fetch(url)));
@@ -32,12 +48,16 @@ export default function RickMortyCharacterCatalog() {
 
       setCharacters(allCharacters);
       setInfo(allInfo);
+
+      setTotalPages(Math.ceil(allInfo?.count / 40));
     }
 
     getCharacters();
-  }, [name]);
+  }, [urlPage, name]);
 
-  const filteredCharacters = characters
+  const shownCharacters = isActive ? favoritesCharacters : characters;
+
+  const filteredCharacters = shownCharacters
     .filter((char) =>
       statusFilter === "All status" ? true : char.status === statusFilter,
     )
@@ -58,19 +78,54 @@ export default function RickMortyCharacterCatalog() {
   function handleFavoriteClick(id) {
     const isFavorite = favoritesCharacters.some((favChar) => favChar.id === id);
 
-    if (isFavorite) {
-      setFavoritesCharacters(favoritesCharacters.filter((favChar) => favChar.id !== id));
-    } else {
+    if (!isFavorite) {
       const favChar = filteredCharacters.find((favChar) => favChar.id === id);
       setFavoritesCharacters([...favoritesCharacters, favChar]);
+      return;
     }
+    setFavoritesCharacters(favoritesCharacters.filter((favChar) => favChar.id !== id));
+  }
+
+  function handleActiveCard(id) {
+    if (activeCard?.id === id) {
+      setActiveCard(null);
+      return;
+    }
+    setActiveCard(filteredCharacters.find((char) => char.id === id));
+  }
+
+  function handleClosedActiveCard(id) {
+    if (activeCard?.id === id) {
+      setActiveCard(null);
+      return;
+    }
+  }
+
+  console.log(activeCard);
+
+  function handleShowFavChar() {
+    if (!isActive) {
+      setIsActive(true);
+      return;
+    }
+    setIsActive(false);
   }
 
   function handleCharSearch(e) {
     setName(e.target.value);
   }
 
-  console.log(name);
+  function handleInputReset() {
+    setName("");
+  }
+
+  function handleBtnNextChar() {
+    setUrlPage(urlPage + 1);
+  }
+
+  function handleBtnPrevChar() {
+    setUrlPage(urlPage - 1);
+  }
 
   return (
     <div className={styles[`app_character-catalog`]}>
@@ -78,7 +133,13 @@ export default function RickMortyCharacterCatalog() {
         <h1 className={styles["app-title"]}>Rick & Morty — character catalog</h1>
       </header>
 
-      <main className={styles["app_main-container"]}>
+      <main
+        className={
+          activeCard === null
+            ? styles["app_main-container-active-card-hidden"]
+            : styles["app_main-container"]
+        }
+      >
         <InfoCharacters
           characters={characters}
           favoritesCharacters={favoritesCharacters}
@@ -86,9 +147,15 @@ export default function RickMortyCharacterCatalog() {
           info={info}
         />
 
-        <SearchCharacters onChange={handleCharSearch} value={name} />
+        <SearchCharacters
+          onChange={handleCharSearch}
+          value={name}
+          onClick={handleInputReset}
+        />
 
         <SortingCharacters
+          onClick={handleShowFavChar}
+          isActive={isActive}
           characters={characters}
           statusFilter={statusFilter}
           setStatusFilter={setStatusFilter}
@@ -98,15 +165,45 @@ export default function RickMortyCharacterCatalog() {
           setSpeciesFilter={setSpeciesFilter}
           sorting={sorting}
           setSorting={setSorting}
+          favoritesCharacters={favoritesCharacters}
         />
 
         <CharactersCard
-          isActive={isActive}
           handleFavoriteClick={handleFavoriteClick}
           characters={filteredCharacters}
           favoritesCharacters={favoritesCharacters}
+          handleActiveCard={handleActiveCard}
         />
       </main>
+
+      <div className={styles["pagination-container"]}>
+        <Button
+          disabled={urlPage === 1}
+          btnClassName={
+            urlPage === 1 ? styles["disabled-btn"] : styles["pagination_btn-left"]
+          }
+          iconClassName={urlPage === 1 ? "" : styles["pagination_icon-left"]}
+          Icon={ArrowLeftIcon}
+          onClick={handleBtnPrevChar}
+        />
+        {urlPage} of {totalPages}
+        <Button
+          disabled={urlPage === totalPages}
+          btnClassName={
+            urlPage === totalPages
+              ? styles["disabled-btn"]
+              : styles["pagination_btn-right"]
+          }
+          iconClassName={urlPage === totalPages ? "" : styles["pagination_icon-right"]}
+          Icon={ArrowRightIcon}
+          onClick={handleBtnNextChar}
+        />
+      </div>
+
+      <ActiveCard
+        activeCard={activeCard}
+        handleClosedActiveCard={handleClosedActiveCard}
+      />
     </div>
   );
 }
