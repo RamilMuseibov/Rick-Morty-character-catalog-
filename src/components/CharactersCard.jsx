440583727afdd7518107006bca6039507233fe75
Ramil <Ramilmys@gmail.com>
2026-02-13 18:31:55 +0700

    Добавлена проверка на отсутствие персонажей

--- src/components/CharactersCard.jsx
+++ src/components/CharactersCard.jsx
@@ -11,51 +11,55 @@ export default function CharactersCard({
 }) {
   return (
     <div className={styles["characters-catalog"]}>
-      {characters?.map((character) => {
-        const status = character.status.toLowerCase();
-        const isFavorite = favoritesCharacters.some(
-          (favChar) => favChar.id === character.id,
-        );
+      {characters.length === 0 ? (
+        <div className={styles["error_characters-undefined"]}>Персонажи не найдeны</div>
+      ) : (
+        characters.map((character) => {
+          const status = character.status.toLowerCase();
+          const isFavorite = favoritesCharacters.some(
+            (favChar) => favChar.id === character.id,
+          );
 
-        return (
-          <div key={character.id} className={styles["characters-card"]}>
-            <img className={styles["character-card-image"]} src={character.image} />
+          return (
+            <div key={character.id} className={styles["characters-card"]}>
+              <img className={styles["character-card-image"]} src={character.image} />
 
-            <Button
-              btnClassName={styles["character-card_btn-favorites"]}
-              Icon={StarIcon}
-              iconClassName={
-                isFavorite
-                  ? styles["character-card_icon-favorites-active"]
-                  : styles["character-card_icon-favorites"]
-              }
-              iconProps={{ filled: isFavorite, colorFilled: "#57cb60" }}
-              onClick={() => handleFavoriteClick(character.id)}
-            />
+              <Button
+                btnClassName={styles["character-card_btn-favorites"]}
+                Icon={StarIcon}
+                iconClassName={
+                  isFavorite
+                    ? styles["character-card_icon-favorites-active"]
+                    : styles["character-card_icon-favorites"]
+                }
+                iconProps={{ filled: isFavorite, colorFilled: "#57cb60" }}
+                onClick={() => handleFavoriteClick(character.id)}
+              />
 
-            <span
-              className={
-                styles[
-                  status === "alive"
-                    ? "character-card_status-alive"
-                    : status === "dead"
-                      ? "character-card_status-dead"
-                      : "character-card_status-unknown"
-                ]
-              }
-            >
-              {character.status}
-            </span>
-
-            <div className={styles[""]}>
-              <span className={styles["character-card_name"]}>{character.name}</span>
-              <span className={styles["character-card_gender"]}>
-                {character.species} • {character.gender}
+              <span
+                className={
+                  styles[
+                    status === "alive"
+                      ? "character-card_status-alive"
+                      : status === "dead"
+                        ? "character-card_status-dead"
+                        : "character-card_status-unknown"
+                  ]
+                }
+              >
+                {character.status}
               </span>
+
+              <div className={styles[""]}>
+                <span className={styles["character-card_name"]}>{character.name}</span>
+                <span className={styles["character-card_gender"]}>
+                  {character.species} • {character.gender}
+                </span>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 }
