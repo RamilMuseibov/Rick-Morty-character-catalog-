bb26acffa80771713b5b3bf59ff0f31ea47c72ee
Ramil <Ramilmys@gmail.com>
2026-02-13 21:27:52 +0700

    Дописаны пропсы

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
-export default function Input({ className, type, placeholder, onChange }) {
+export default function Input({ className, type, placeholder, onChange, value }) {
   return (
     <input
       className={className}
       type={type}
       placeholder={placeholder}
+      value={value}
       onChange={onChange}
     />
   );
