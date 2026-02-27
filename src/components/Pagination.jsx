aaf1a1f2726c2c2e2f16a3119adb5e8383478c3b
Ramil <Ramilmys@gmail.com>
2026-02-27 08:04:12 +0700

    Добавлен новый компонент, куда вынесена пагинация

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "../styles/character-catalog.module.css";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
+import ArrowLeftIcon from "../icons/ArrowLeftIcon";
+import Button from "./Button";
+import { useCharacters } from "../store/characters";
+import { useIsActive } from "../store/isActive";
+
+export default function Pagination() {
+  const characters = useCharacters((state) => state.characters);
+  const isActive = useIsActive((state) => state.isActive);
+  const urlPage = useCharacters((state) => state.urlPage);
+  const setUrlPage = useCharacters((state) => state.setUrlPage);
+  const totalPages = useCharacters((state) => state.totalPages);
+
+  return (
+    <div
+      className={
+        isActive || characters.length === 0
+          ? styles["pagination-container-hidden"]
+          : styles["pagination-container"]
+      }
+    >
+      <Button
+        disabled={urlPage === 1}
+        btnClassName={
+          urlPage === 1 ? styles["disabled-btn"] : styles["pagination_btn-left"]
+        }
+        iconClassName={urlPage === 1 ? "" : styles["pagination_icon-left"]}
+        Icon={ArrowLeftIcon}
+        onClick={() => setUrlPage(urlPage - 1)}
+      />
+      {urlPage} of {totalPages}
+      <Button
+        disabled={urlPage === totalPages}
+        btnClassName={
+          urlPage === totalPages ? styles["disabled-btn"] : styles["pagination_btn-right"]
+        }
+        iconClassName={urlPage === totalPages ? "" : styles["pagination_icon-right"]}
+        Icon={ArrowRightIcon}
+        onClick={() => setUrlPage(urlPage + 1)}
+      />
+    </div>
+  );
+}
